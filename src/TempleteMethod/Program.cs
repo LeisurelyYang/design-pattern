891874d9671415226c78ec10d42ce1b0dad8737c
Yaphet <yyc266@163.com>
2019-02-28 22:13:39 +0800

    模板方法模式

---
+++ src/TempleteMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TempleteMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass A = new ConcreteClassA();
+            AbstractClass B = new ConcreteClassB();
+
+            A.TempleteMethod();
+            B.TempleteMethod();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class AbstractClass
+    {
+        public abstract void A();
+        public abstract void B();
+
+        public void TempleteMethod()
+        {
+            A();
+            B();
+            Console.WriteLine("模板方法内");
+        }
+    }
+
+    class ConcreteClassA : AbstractClass
+    {
+        public override void A()
+        {
+            Console.WriteLine("AA");
+        }
+
+        public override void B()
+        {
+            Console.WriteLine("BB");
+        }
+    }
+
+    class ConcreteClassB : AbstractClass
+    {
+        public override void A()
+        {
+            Console.WriteLine("BA");
+        }
+
+        public override void B()
+        {
+            Console.WriteLine("BB");
+        }
+    }
+}
