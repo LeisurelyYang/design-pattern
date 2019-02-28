0194f9e4b8cac853eacc0b3e616e3f67006bedd8
yangyucheng <yucheng1@7net.cc>
2019-02-28 09:36:25 +0800

    工厂方法模式

---
+++ src/FactoryMethod/LeiFeng.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class LeiFeng
+    {
+        public void WeiFan()
+        {
+            Console.WriteLine("喂饭");
+        }
+
+        public void WeiYao()
+        {
+            Console.WriteLine("喂药");
+        }
+    }
+}
---
+++ src/FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    //工厂方法
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILeiFengFactory factory = new StudentLeiFengFactory();
+            LeiFeng leifeng = factory.CreateLeiFeng();
+            leifeng.WeiFan();
+            Console.ReadKey();
+        }
+    }
+}
