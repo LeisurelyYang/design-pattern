901c2bd8d0cad7f4997f1c70fc71580933931de5
Yaphet <yyc266@163.com>
2019-03-01 21:54:01 +0800

    建造者模式

---
+++ src/Builder/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ConcreteBuild1 c1 = new ConcreteBuild1();
+            ConcreteBuild2 c2 = new ConcreteBuild2();
+
+            Director d = new Director();
+
+            d.Contruct(c1);
+            c1.GetResult().Show();
+
+            d.Contruct(c2);
+            c2.GetResult().Show();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Director
+    {
+        public void Contruct(Builder builde)
+        {
+            builde.BuildA();
+            builde.BuildB();
+        }
+    }
+
+    //建造的产品
+    class Product
+    {
+        IList<string> parts = new List<string>();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("建造结果");
+            foreach (var item in parts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    abstract class Builder
+    {
+        public abstract void BuildA();
+
+        public abstract void BuildB();
+
+        public abstract Product GetResult();
+    }
+
+    class ConcreteBuild1 : Builder
+    {
+        Product product = new Product();
+        public override void BuildA()
+        {
+            product.Add("瘦子部件A");
+        }
+
+        public override void BuildB()
+        {
+            product.Add("瘦子部件B");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+
+    class ConcreteBuild2 : Builder
+    {
+        Product product = new Product();
+        public override void BuildA()
+        {
+            product.Add("胖子部件A");
+        }
+
+        public override void BuildB()
+        {
+            product.Add("胖子部件B");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+}
---
+++ src/Facade/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Facade fade = new Facade();
+
+            fade.MethodA();
+            fade.MethodB();
+            Console.ReadKey();
+        }
+    }
+
+    class Facade
+    {
+        SubSystem1 sys1;
+        SubSystem2 sys2;
+        SubSystem3 sys3;
+        public Facade()
+        {
+            this.sys1 = new SubSystem1();
+            this.sys2 = new SubSystem2();
+            this.sys3 = new SubSystem3();
+        }
+
+        public void MethodA()
+        {
+            sys1.Method1();
+            sys2.Method2();
+        }
+
+        public void MethodB()
+        {
+            sys2.Method2();
+            sys3.Method3();
+        }
+    }
+
+    class SubSystem1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("方法1");
+        }
+    }
+
+    class SubSystem2
+    {
+        public void Method2()
+        {
+            Console.WriteLine("方法2");
+        }
+    }
+
+    class SubSystem3
+    {
+        public void Method3()
+        {
+            Console.WriteLine("方法3");
+        }
+    }
+}
