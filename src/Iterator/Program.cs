405103e8983eacce138a4ec4ea9e54129b4b67c8
Yaphet <yyc266@163.com>
2019-03-05 21:01:28 +0800

    迭代器模式

---
+++ src/Iterator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "大神";
+            a[1] = "小神";
+            a[2] = "老神";
+
+            Iterator i = new ConcreteIterator(a);
+
+            object item = i.First();
+
+            while (!i.IsDone())
+            {
+                Console.WriteLine($"{i.CurrentItem()} 请买车票");
+                i.Next();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
