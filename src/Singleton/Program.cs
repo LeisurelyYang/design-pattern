80b2fa895366b3deae87ffad4349089c936251bc
Yaphet <yyc266@163.com>
2019-03-05 21:56:55 +0800

    单例模式

---
+++ src/Singleton/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            if (s1 == s2)
+            {
+                Console.WriteLine("两个对象是相同的实例。");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Singleton
+    {
+        private static readonly object objLock = new object();
+
+        private static Singleton singleton = null;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (singleton == null)
+            {
+
+                lock (objLock)
+                {
+                    if (singleton == null)
+                    {
+                        return singleton = new Singleton();
+                    }
+                }
+            }
+
+            return singleton;
+        }
+    }
+}
