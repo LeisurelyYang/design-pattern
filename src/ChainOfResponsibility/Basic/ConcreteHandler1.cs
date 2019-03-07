b5691780f76b5ef8199715daa5623516e8cbaf22
yangyucheng <yucheng1@7net.cc>
2019-03-07 09:01:14 +0800

    职责链模式

---
+++ src/ChainOfResponsibility/Basic/ConcreteHandler1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Basic
+{
+    class ConcreteHandler1 : Handle
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine($"{this.GetType().Name}处理请求{request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
